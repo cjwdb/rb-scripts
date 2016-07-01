53a5f56e89ffd361bf823faf91bce6b5cd7122fa
Chris de Bruin <chris.debruin@redballoon.com.au>
2016-07-02 04:00:46 +1000

    Initial setup

---
+++ cloneallrepos.sh
@@ -0,0 +1,17 @@
+git clone git@github.com:RedBalloonIT/CoreApi.git
+git clone git@github.com:RedBalloonIT/SupplierUI.git
+git clone git@github.com:RedBalloonIT/AlfredUI.git
+git clone git@github.com:RedBalloonIT/LiveAvailabilityAPI.git
+git clone git@github.com:RedBalloonIT/ECommerceUI.git
+git clone git@github.com:RedBalloonIT/Gestalt.git
+git clone git@github.com:RedBalloonIT/morpheus-web.git
+git clone git@github.com:RedBalloonIT/AU-Ecommerce-AutomationTests.git 
+git clone git@github.com:RedBalloonIT/LiveAvailabilityDomain.git
+git clone git@github.com:RedBalloonIT/rb-slick-generator.git
+git clone git@github.com:RedBalloonIT/LiveAvailabilitySlickDao.git
+git clone git@github.com:RedBalloonIT/RBAuthClient.git
+git clone git@github.com:RedBalloonIT/morpheus-cucumber.git
+git clone git@github.com:RedBalloonIT/ui-library.git
+git clone git@github.com:RedBalloonIT/consumer.git
+git clone git@github.com:RedBalloonIT/Legacy-Task.git
+
---
+++ getremoteurls.sh
@@ -0,0 +1,20 @@
+cd ~/git/redballoon/CoreApi
+git remote -v
+cd ~/git/redballoon/ECommerceUI
+git remote -v
+cd ~/git/redballoon/AlfredUI
+git remote -v
+cd ~/git/redballoon/Gestalt
+git remote -v
+cd ~/git/redballoon/Legacy-Task
+git remote -v
+cd ~/git/redballoon/LiveAvailabilityAPI
+git remote -v
+cd ~/git/redballoon/LiveAvailabilityDomain
+git remote -v
+cd ~/git/redballoon/LiveAvailabilitySlickDao
+git remote -v
+cd ~/git/redballoon/SupplierUI
+git remote -v
+cd ~/git/redballoon/morpheus-web
+git remote -v
