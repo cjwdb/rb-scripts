c5974b0d212af217ee6597fcce07504ff99d9844
Chris de Bruin <chris@chris.fedora>
2016-07-02 05:11:19 +1000

    hard coded target dirs for cloneall

--- cloneallrepos.sh
+++ cloneallrepos.sh
@@ -1,17 +1,17 @@
-git clone git@github.com:RedBalloonIT/CoreApi.git
-git clone git@github.com:RedBalloonIT/SupplierUI.git
-git clone git@github.com:RedBalloonIT/AlfredUI.git
-git clone git@github.com:RedBalloonIT/LiveAvailabilityAPI.git
-git clone git@github.com:RedBalloonIT/ECommerceUI.git
-git clone git@github.com:RedBalloonIT/Gestalt.git
-git clone git@github.com:RedBalloonIT/morpheus-web.git
-git clone git@github.com:RedBalloonIT/AU-Ecommerce-AutomationTests.git 
-git clone git@github.com:RedBalloonIT/LiveAvailabilityDomain.git
-git clone git@github.com:RedBalloonIT/rb-slick-generator.git
-git clone git@github.com:RedBalloonIT/LiveAvailabilitySlickDao.git
-git clone git@github.com:RedBalloonIT/RBAuthClient.git
-git clone git@github.com:RedBalloonIT/morpheus-cucumber.git
-git clone git@github.com:RedBalloonIT/ui-library.git
-git clone git@github.com:RedBalloonIT/consumer.git
-git clone git@github.com:RedBalloonIT/Legacy-Task.git
+git clone git@github.com:RedBalloonIT/CoreApi.git ~/git/redballoon/CoreApi
+git clone git@github.com:RedBalloonIT/SupplierUI.git ~/git/redballoon/SupplierUI
+git clone git@github.com:RedBalloonIT/AlfredUI.git ~/git/redballoon/AlfredUI
+git clone git@github.com:RedBalloonIT/LiveAvailabilityAPI.git ~/git/redballoon/LiveAvailabilityAPI
+git clone git@github.com:RedBalloonIT/ECommerceUI.git ~/git/redballoon/ECommerceUI
+git clone git@github.com:RedBalloonIT/Gestalt.git ~/git/redballoon/Gestalt
+git clone git@github.com:RedBalloonIT/morpheus-web.git ~/git/redballoon/morpheus-web
+git clone git@github.com:RedBalloonIT/AU-Ecommerce-AutomationTests.git ~/git/redballoon/AU-Ecommerce-AutomationTests
+git clone git@github.com:RedBalloonIT/LiveAvailabilityDomain.git ~/git/redballoon/LiveAvailabilityDomain
+git clone git@github.com:RedBalloonIT/rb-slick-generator.git ~/git/redballoon/rb-slick-generator
+git clone git@github.com:RedBalloonIT/LiveAvailabilitySlickDao.git ~/git/redballoon/LiveAvailabilitySlickDao
+git clone git@github.com:RedBalloonIT/RBAuthClient.git ~/git/redballoon/RBAuthClient
+git clone git@github.com:RedBalloonIT/morpheus-cucumber.git ~/git/redballoon/morpheus-cucumber
+git clone git@github.com:RedBalloonIT/ui-library.git ~/git/redballoon/ui-library
+git clone git@github.com:RedBalloonIT/consumer.git ~/git/redballoon/consumer
+git clone git@github.com:RedBalloonIT/Legacy-Task.git ~/git/redballoon/Legacy-Task
 
